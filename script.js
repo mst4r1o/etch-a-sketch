6dbd5463862b2cc09e1035a67c4ee59f2a7ab89e
mst4r1o <mst4r1o@gmail.com>
2024-03-13 16:30:57 +0800

    Color boxes when mouse hover over
    Boxes can now be colored when the mouse hovers over them. For additional
    aesthetic, the cursor will change to a pointer to affirm users that it
    is intended and something is happening. Colors are also randomised but
    I don't understand the other extra credit where you make the Colors
    darker and darker till black as then it wouldn't feel like the color is
    random.

--- script.js
+++ script.js
@@ -9,18 +9,23 @@ function createDiv() {
     return gridDiv;
 }
 
-let count = 0;
+function randomNum(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
 for (let i = 0; i < 16; i++) {
     const flexContainer = document.createElement("div");
     flexContainer.className = "flex-container";
 
     for (let j = 0; j < 16; j++) {
         let newDiv = createDiv();
+        newDiv.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = `rgb(${randomNum(255)}, ${randomNum(255)}, ${randomNum(255)})`;
+            e.target.style.cursor = "pointer";
+        })
         flexContainer.appendChild(newDiv);
-        console.log(++count);
     }
     container.appendChild(flexContainer);
 }
 
 body.appendChild(container);
-console.log("hi");
