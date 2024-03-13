496de693aa6148d7af0315f8b8dd268ce254df70
mst4r1o <mst4r1o@gmail.com>
2024-03-13 17:05:59 +0800

    Add button to change grid size and clean up code
    Button created in the centre above the grid to prompt user for a number
    to change the grid size when pressed. It will allow the previous grid to
    be erased and create a new grid of a different size the user inputs.
    
    Code has also been cleaned up to be more modular, especially the
    Javascript file, to prevent repetition of code. New createGrid()
    function made to fulfil the aforementioned purpose.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const body = document.querySelector("body");
-const container = document.createElement("div");
-container.className = "container";
+const button = document.querySelector(".button");
 
 function createDiv() {
     const gridDiv = document.createElement("div");
@@ -13,19 +12,36 @@ function randomNum(max) {
     return Math.floor(Math.random() * max) + 1;
 }
 
-for (let i = 0; i < 16; i++) {
-    const flexContainer = document.createElement("div");
-    flexContainer.className = "flex-container";
+function createGrid(size) {
+    const container = document.createElement("div");
+    container.className = "container";
 
-    for (let j = 0; j < 16; j++) {
-        let newDiv = createDiv();
-        newDiv.addEventListener("mouseenter", (e) => {
-            e.target.style.backgroundColor = `rgb(${randomNum(255)}, ${randomNum(255)}, ${randomNum(255)})`;
-            e.target.style.cursor = "pointer";
-        })
-        flexContainer.appendChild(newDiv);
+    for (let i = 0; i < size; i++) {
+        const flexContainer = document.createElement("div");
+        flexContainer.className = "flex-container";
+
+        for (let j = 0; j < size; j++) {
+            let newDiv = createDiv();
+            newDiv.addEventListener("mouseenter", (e) => {
+                e.target.style.backgroundColor = `rgb(${randomNum(255)}, ${randomNum(255)}, ${randomNum(255)})`;
+                e.target.style.cursor = "pointer";
+            })
+            flexContainer.appendChild(newDiv);
+        }
+        container.appendChild(flexContainer);
     }
-    container.appendChild(flexContainer);
+    return container;
 }
+let gridContainer = createGrid(16);
+body.insertBefore(gridContainer, button.nextSibling);
 
-body.appendChild(container);
+button.addEventListener("click", () => {
+    console.log("clicked");
+    let size = 101;
+    while (size > 100 || size < 0) {
+        size = prompt("How many squares per side would you like?");
+    }
+    body.removeChild(gridContainer);
+    gridContainer = createGrid(size);
+    body.insertBefore(gridContainer, button.nextSibling);
+})
